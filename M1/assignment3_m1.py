c9dcbfecf2bae48469e12246b09c064aacc86980
Nishchay Agrawal <agrawal.nishchay5@gmail.com>
2023-02-15 18:42:37 -0800

    Added skeleton code for assignment 3 milestone 1

---
+++ M1/assignment3_m1.py
@@ -0,0 +1,15 @@
+import time
+import os
+
+def get_file_names():
+    pass
+
+def process_data(file_names):
+    pass
+
+if __name__ == "__main__":
+    start_time = time.time()
+    file_names = get_file_names()
+    process_data(file_names)
+    end_time = time.time()
+    print("Total execution time: " + str(end_time - start_time))
