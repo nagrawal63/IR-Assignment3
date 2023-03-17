71e85e840f70a7341c8401c011b0166151036d5f
godadevi <gaddanki@uci.edu>
2023-03-16 17:36:07 -0700

    using n-gram

--- M2/assignment3_m2.py
+++ M2/assignment3_m2.py
@@ -1,7 +1,7 @@
 from assignment3_m1 import tokenize_content
-from InvertedIndexLoader import loadInvertedIndexFromFile , getIndexDataAllTokens
+from InvertedIndexLoader import loadInvertedIndexFromFile, getIndexDataAllTokens,getIndexDataAllTokensBigram,getIndexDataAllTokensTrigram
 import time
-import json
+import json, math
 import numpy as np
 from enum import IntEnum
 from numpy import dot
@@ -9,6 +9,11 @@ from numpy.linalg import norm
 # from sklearn.metrics.pairwise import cosine_similarity
 from flask import Flask
 from flask_cors import CORS
+from assignment3_m1 import tokenize_content,tokenize_content_without_stopwords
+from assignment3_m1 import generate_trigram_tokens
+from assignment3_m1 import generate_bigram_tokens
+
+blacklist_urls = set(["http://mondego.ics.uci.edu/datasets/maven-contents.txt"])
 
 app = Flask(__name__)
 CORS(app)
@@ -16,15 +21,21 @@ with open("./url_docID_map.json") as f:
     doc2id = json.load(f)
 with open("./docID_url_map.json") as f:
     id2doc = json.load(f)
-with open('../M1/page_quality_features.json') as f:
+with open('./page_quality_features.json') as f:
     doc2features = json.load(f)
-with open('./dicId_title_map.json') as f:
-    id2title= json.load(f)
 
-def process_query(query):
-    query_tokens = tokenize_content(query)
+
+
+def process_query(query, type):
+    if type == 3:
+        query_tokens = generate_trigram_tokens(tokenize_content_without_stopwords(query))
+    elif type == 2:
+        query_tokens = generate_bigram_tokens(tokenize_content_without_stopwords(query))
+    else:
+        query_tokens = tokenize_content(query)
     print(f"{query_tokens}")
     return query_tokens
+
 
 class ImportanceEnum(IntEnum):
     TITLE = 1
@@ -35,53 +46,90 @@ class ImportanceEnum(IntEnum):
     NORMAL = 6
     IMPORTANT = 7
 
-def merge_inverted_index(datal,doc2features):
+
+def merge_inverted_index(datal, doc2features, tokens):
     importance_map = {ImportanceEnum.NORMAL: 1, ImportanceEnum.B: 2, ImportanceEnum.H3: 3,
-                           ImportanceEnum.H2: 4, ImportanceEnum.H1: 5, ImportanceEnum.TITLE: 6}
+                      ImportanceEnum.H2: 4, ImportanceEnum.H1: 5, ImportanceEnum.TITLE: 6}
     final_page = {}
-    queryv =[1] *len(datal)
-    
-    for i,td in enumerate(datal):
-        #queryv[i] = 1/ len(td)
+    queryv = [1] * len(datal)
+
+    for i, td in enumerate(datal):
+        # queryv[i] = 1/ len(td)
         for d in td:
             if d.docId in final_page:
                 final_page[d.docId][i] = d.tfidf
             else:
-                final_page[d.docId] = [0] * (len(datal)+1 )  #[len(td)TODO]+1 page features # initialize with zero vector
+                final_page[d.docId] = [0] * (
+                            len(datal) + 1)  # [len(td)TODO]+1 page features # initialize with zero vector
                 final_page[d.docId][i] = d.tfidf
     queryv.extend([0.01])
+
+    # Get documents which are target pages of some anchor texts
+    joined_query = ' '.join(tokens)
+    anchorTargetPages = None
+
+
     for d in final_page:
-        final_page[d][-1] = doc2features[str(d)]['pagein']/len(doc2features) 
+        final_page[d][-1] = doc2features[str(d)]['pagein'] / len(doc2features)
 
-    return sorted({k:dot(queryv,v) for k,v in final_page.items() if 0 not in v}.items(),key=lambda x :x[1],reverse=True) # [TODO] & option adding ? 
 
-def retrieve_pages(tokens,doc2features):
-    datal = getIndexDataAllTokens(tokens)
-    pages = merge_inverted_index(datal,doc2features)
+    return sorted({k: dot(queryv, v) for k, v in final_page.items() if 0 not in v}.items(), key=lambda x: x[1],
+                  reverse=True)  # [TODO] & option adding ?
+
+
+def retrieve_pages(tokens, doc2features,type):
+    if type == 3:
+        datal = getIndexDataAllTokensTrigram(tokens)
+    elif type == 2:
+        datal = getIndexDataAllTokensBigram(tokens)
+    else:
+        datal = getIndexDataAllTokens(tokens)
+    pages = merge_inverted_index(datal, doc2features, tokens)
+    pages = [page for page in pages if str(page[0]) not in blacklist_urls]
     return pages[:10]
 
-@app.route('/main',methods=['GET'])
+
+@app.route('/main', methods=['GET'])
 def main():
     from flask import request
     query = request.args.get('query')
     tokens = process_query(query)
-    pages = retrieve_pages(tokens,doc2features)   
-    return json.dumps({i:(id2title[str(p[0])],id2doc[str(p[0])]) for i,p in enumerate(pages)})
-    
+    pages = retrieve_pages(tokens, doc2features)
+    return json.dumps({i: id2doc[str(p[0])] for i, p in enumerate(pages)})
+
+
 if __name__ == "__main__":
     while True:
         with open("./url_docID_map.json") as f:
             doc2id = json.load(f)
         with open("./docID_url_map.json") as f:
             id2doc = json.load(f)
-        with open('../M1/page_quality_features.json') as f:
+        with open('./page_quality_features.json') as f:
             doc2features = json.load(f)
+
         print("Enter Query:")
         query = input()
         start_time = time.time()
-        tokens = process_query(query)
-        pages = retrieve_pages(tokens,doc2features)
+        pages = []; pages_trigram=[];pages_bigram = []
+        tokens = process_query(query, 3)
+        pages_trigram = retrieve_pages(tokens, doc2features,3)
+        # print(pages_trigram)
+        if len(pages_trigram)<5:
+            tokens = process_query(query, 2)
+            pages_bigram = (retrieve_pages(tokens, doc2features,2))
+            # print(pages_bigram)
+        if len(pages_trigram) + len(pages_bigram) < 5:
+            tokens = process_query(query, 1)
+            pages = (retrieve_pages(tokens, doc2features,1))
+            # print(pages)
+        for p in pages_trigram:
+            # print(p,id2doc[str(p[0])])
+            print(id2doc[str(p[0])])
+        for p in pages_bigram:
+            # print(p,id2doc[str(p[0])])
+            print(id2doc[str(p[0])])
         for p in pages:
-            print(p,id2doc[str(p[0])],doc2features[str(p[0])]) 
+            # print(p,id2doc[str(p[0])])
+            print(id2doc[str(p[0])])
         end_time = time.time()
-        print("Processing query({}) took {} seconds".format(query,end_time-start_time))
+        print("Processing query({}) took {} seconds".format(query, end_time - start_time))
--- M2/query_processor.py
+++ M2/query_processor.py
@@ -1,13 +1,162 @@
-from InvertedIndex import InvertedIndex, Postings
+from InvertedIndexLoader import getIndexEntry
+from InvertedIndexLoader import getIndexDataAllTokens
 import time
+from assignment3_m1 import tokenize_content,tokenize_content_without_stopwords
+from assignment3_m1 import generate_trigram_tokens
+from assignment3_m1 import generate_bigram_tokens
+import numpy as np
+import json, math
+from collections import defaultdict
 
-def process_query(query):
-    pass
+TOTAL_DOCS = 55393
+rankingTechnique = ["tfIdfCosineSimilarity", "pageRank"]
+
+
+def get_tf_idf_scores(query_index, docID):
+    for index in query_index:
+        for web_page in query_index[index]:
+            if web_page.docId == docID:
+                return web_page.tfidf_improved
+    return 0
+
+
+def loadDocID_to_URL_map():
+    docDict = {}
+    with open("docID_url_map.json", 'r') as f:
+        docDict = json.load(f)
+    return docDict
+
+
+def load_page_rank():
+    pageRankDict = {}
+    with open("page_quality_features.json", 'r') as f:
+        pageRankDict = json.load(f)
+    return pageRankDict
+
+
+# def rankingResults(rankingTechniques, pageRankDict, queryIndexes, commonDocs, queryScore):
+#     rankedDocs = [(doc,) for doc in commonDocs]
+#     if len(rankingTechniques) == 0:
+#         return rankedDocs
+#     i = 0
+#     while i < len(rankingTechniques):
+#         rankingTechnique = rankingTechniques[i]
+#         if rankingTechnique == "tfIdfSum":
+#             tfIdfMatrix = get_tf_idf_scores(commonDocs, queryIndexes)
+#             tfIdfScore = {k: sum(v) for k, v in tfIdfMatrix.items()}
+#             rankedDocs = [(rankedDocs[i] + (tfIdfScore[rankedDocs[i][0]],)) for i in range(len(rankedDocs))]
+#         elif rankingTechnique == "tfIdfCosineSimilarity":
+#             tfIdfMatrix = get_tf_idf_scores(commonDocs, queryIndexes)
+#             cosineSimilarityVec = {k: np.dot(v, queryScore) / (np.linalg.norm(v) * np.linalg.norm(queryScore)) for k, v
+#                                    in tfIdfMatrix.items()}
+#             rankedDocs = [(rankedDocs[i] + (cosineSimilarityVec[rankedDocs[i][0]],)) for i in range(len(rankedDocs))]
+#         elif rankingTechnique == "pageRank":
+#             pageRankDict = load_page_rank()
+#             commonDocsPageRank = {docId: pageRankDict[str(docId)]["pagerank"] for docId in commonDocs}
+#             rankedDocs = [(rankedDocs[i] + (commonDocsPageRank[rankedDocs[i][0]],)) for i in range(len(rankedDocs))]
+#         elif rankingTechnique == "docScore":
+#             tfIdfMatrix = get_tf_idf_scores(commonDocs, queryIndexes)
+#             docScores = {k: np.dot(v, queryScore) for k, v in tfIdfMatrix.items()}
+#             rankedDocs = [(rankedDocs[i] + (docScores[rankedDocs[i][0]],)) for i in range(len(rankedDocs))]
+#         i += 1
+#     rankedDocs.sort(key=lambda x: (x[1:]), reverse=True)
+#     return rankedDocs
+
+
+def process_query(query, pageRanksDict):
+    tokenized_query = tokenize_content(query)
+    query_freq = {}
+    # for token in tokenized_query:
+    #     if token in query_freq.keys():
+    #         query_freq[token] += 1
+    #     else:
+    #         query_freq[token] = 1
+    # bigrams = generate_bigram_tokens(tokenize_content_without_stopwords(query))
+    # for token in bigrams:
+    #     if token in query_freq.keys():
+    #         query_freq[token] += 10
+    #     else:
+    #         query_freq[token] = 10
+
+    trigrams = generate_trigram_tokens(tokenize_content_without_stopwords(query))
+    for token in trigrams:
+        if token in query_freq.keys():
+            query_freq[token] += 20
+        else:
+            query_freq[token] = 20
+
+    # print(query_freq)
+
+    query_indexes = []; common_docs = [];query_docs_len = [];query_docs = [];docs = []
+    indexes = getIndexDataAllTokens(sorted(query_freq.keys()))
+    token_index = defaultdict()
+    # print(token_indexes)
+    for token,index in zip(sorted(query_freq.keys()),indexes):
+        token_index[token] = index
+        # print(token_index)
+        if token_index is not None:
+            query_indexes.append(token_index)
+            docs = list(map(lambda x: x.docId, token_index[token]))
+            common_docs += docs
+            query_docs.append(list(docs))
+            query_docs_len.append(len(docs))
+        else:
+            del query_freq[token]
+
+    if len(common_docs) == 0:
+        print("sorry, nothing matches, please check your query")
+        return None
+    print("after deletion", query_freq)
+    common_docs = list(set(common_docs))
+    query_tfidf_array = calculate_query_tf_idf_score(query_freq,query_docs_len)
+
+    common_docs_tfidf_map = calculate_tf_idf_array_common_docs(query_freq,common_docs,query_indexes,query_docs)
+    rankedDocs = calculate_cosineSimilarityVec(common_docs_tfidf_map, common_docs, query_tfidf_array)
+
+    # rankedDocs = rankingResults(rankingTechnique, pageRanksDict, query_indexes, common_docs, np.array(query_score))
+    pageRankDict = load_page_rank()
+    commonDocsPageRank = {docId: pageRankDict[str(docId)]["pagerank"] for docId in [rankedDoc[0] for rankedDoc in rankedDocs]}
+
+    rankedDocs = [(rankedDocs[i] + (commonDocsPageRank[rankedDocs[i][0]],)) for i in range(len(rankedDocs))]
+    rankedDocs.sort(key=lambda x: (x[1:]), reverse=True)
+    query_output = []
+    docID_to_URL_map = loadDocID_to_URL_map()
+    for i in range(len(rankedDocs[:5])):
+        query_output.append(docID_to_URL_map[str(rankedDocs[i][0])])
+    return query_output
+
+def calculate_query_tf_idf_score(query_freq,query_docs_len):
+    query_score = []
+    for token,docs_len in zip(sorted(query_freq.keys()),query_docs_len):
+        # tf idf score calculation for the query
+        tf_idf = (1 + math.log(query_freq[token])) * math.log(TOTAL_DOCS/docs_len)
+        query_score.append(tf_idf)
+    return query_score
+
+def calculate_tf_idf_array_common_docs(query_freq,common_docs,query_indexes,query_docs):
+    URL_tfidf_array_map = defaultdict(list)
+    for doc in common_docs:
+        for word,query_index,docs in zip(sorted(query_freq.keys()),query_indexes,query_docs):
+            if doc in docs:
+                URL_tfidf_array_map[doc].append(get_tf_idf_scores(query_index,doc))
+            else:
+                URL_tfidf_array_map[doc].append(0)
+    return URL_tfidf_array_map
+
+def calculate_cosineSimilarityVec(common_docs_tfidf_map,common_docs,query_tfidf_array):
+    rankedDocs = []
+    for doc in common_docs:
+        cosine_val = np.dot(common_docs_tfidf_map[doc], query_tfidf_array)/(np.linalg.norm(common_docs_tfidf_map[doc]) * np.linalg.norm(query_tfidf_array))
+        rankedDocs.append((doc,cosine_val))
+    return rankedDocs
 
 if __name__ == "__main__":
     while True:
         query = input()
+        pageRanksDict = load_page_rank()
         start_time = time.time()
-        process_query(query)
+        query_output = process_query(query, pageRanksDict)
         end_time = time.time()
+        for output in query_output:
+            print(output)
         print("Processing query({}) took {} seconds".format(query, str(end_time - start_time)))
